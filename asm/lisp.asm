419e4648011eb3c687260dbd798a730baacb3ae9
Wei-ju Wu <weiju.wu@gmail.com>
2015-07-09 21:45:16 -0700

    cons, car and cdr are implemented

--- asm/lisp.asm
+++ asm/lisp.asm
@@ -1,18 +1,25 @@
         ;; Lisp runtime system, assume for now, we can freely allocate
         
         ;; 1. every value is 16 bit
+        ;; 2. values have a 3 bit tag
+        ;;   a. 0 = cons cell index
+        ;;   b. 1 = integer
+        ;;   c. 2 = char
         ;; 2. never deallocate cons-cells
+        
 
         ;; memory range for cons cells ($c000-$cfff)
         ;; first 2 bytes hold number of cons cells
 CC_BASE=$c000
-CC_OFFSET=2
+CC_OFFSET=4
 CC_NIL=0
 
         ;; memory range for parameters ($8000-$8fff)
 RETVAL=$8000
 ARG1=$8002
 ARG2=$8004
+SCRATCH1=$8ffc
+SCRATCH2=$8ffe
         
         org $1000
         JSR init
@@ -20,17 +27,18 @@ ARG2=$8004
         ;; just as a test
         JSR alloc_cc
         LDA RETVAL              ; get byte offset of cons cell
+        STA SCRATCH1            ; save for later
         STA ARG1                ; set car -> $CAFE
         LDA #$CA
         STA ARG2
         LDA #$FE
         STA ARG2+1
         JSR set_car
-        
 
         ;; just as a test
         JSR alloc_cc
         LDA RETVAL
+        STA SCRATCH2
         STA ARG1                ; set car -> $FACE
         LDA #$FA
         STA ARG2
@@ -38,6 +46,17 @@ ARG2=$8004
         STA ARG2+1
         JSR set_car
 
+        ;; test (cons cc1 cc2)
+        LDA SCRATCH1
+        STA ARG1
+        LDA #0
+        STA ARG1+1
+        LDA SCRATCH2
+        STA ARG2
+        LDA #0
+        STA ARG2+1
+        JSR set_cdr
+        
         BRK
 
         ;; initialize the Lisp runtime
@@ -85,4 +104,28 @@ set_car LDA ARG1                ; cons cell index
         STA CC_BASE,Y           ; y = byte offset within cons cell table
         LDA ARG2+1              ; init value hi-byte
         STA CC_BASE+1,Y
+        RTS
+
+        ;; return cc[ARG1].car
+car     LDA ARG1                ; cons cell index
+        ASL
+        ASL
+        ADC #CC_OFFSET
+        TAY                     ; accu -> Y
+        LDA CC_BASE,Y
+        STA RETVAL
+        LDA CC_BASE+1,Y
+        STA RETVAL+1
+        RTS
+
+        ;; return cc[ARG1].cdr
+cdr     LDA ARG1                ; cons cell index
+        ASL
+        ASL
+        ADC #(CC_OFFSET+2)
+        TAY                     ; accu -> Y
+        LDA CC_BASE,Y
+        STA RETVAL
+        LDA CC_BASE+1,Y
+        STA RETVAL+1
         RTS
